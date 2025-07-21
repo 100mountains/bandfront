4bd33ea9e255821e845ff1eb055ce4a1124cb945
100mountains <root@100mountains.uk>
2025-07-21 22:52:15 +0000

    clean

--- functions.php
+++ functions.php
@@ -3,20 +3,24 @@
  * Storefront Child Theme functions and definitions
  */
 
+/*--------------------------------------------------------------
+# Stylesheets
+--------------------------------------------------------------*/
+
 /**
  * Enqueue parent theme's stylesheet and child theme's stylesheet
  */
 function storefront_child_enqueue_styles() {
     $parent_style = 'storefront-style';
-    
+
     // Enqueue parent theme's stylesheet
     wp_enqueue_style(
-        $parent_style, 
+        $parent_style,
         get_template_directory_uri() . '/style.css',
         array(),
         wp_get_theme('storefront')->get('Version')
     );
-    
+
     // Enqueue child theme's stylesheet with parent's as dependency
     wp_enqueue_style(
         'storefront-child-style',
@@ -27,23 +31,28 @@ function storefront_child_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'storefront_child_enqueue_styles', 11);
 
-// Disable WooCommerce product image zoom
-
-add_action( 'wp', function() {
-    remove_theme_support( 'wc-product-gallery-zoom' );
-});
-
+/*--------------------------------------------------------------
+# WooCommerce Customizations
+--------------------------------------------------------------*/
 
 /**
- * Remove Breadcrumbs
+ * Disable WooCommerce product image zoom
  */
-add_action( 'init', 'storefront_remove_storefront_breadcrumbs' );
+add_action('wp', function() {
+    remove_theme_support('wc-product-gallery-zoom');
+});
 
+/**
+ * Remove WooCommerce breadcrumbs from Storefront theme
+ */
+add_action('init', 'storefront_remove_storefront_breadcrumbs');
 function storefront_remove_storefront_breadcrumbs() {
-    remove_action( 'storefront_before_content', 'woocommerce_breadcrumb', 10 );
+    remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
 }
 
-// Redirect only logged-out users trying to access my-account page
+/**
+ * Redirect only logged-out users trying to access my-account page
+ */
 add_action('template_redirect', function() {
     if (is_account_page() && !is_user_logged_in()) {
         wp_redirect(home_url('/login/'));
@@ -51,19 +60,34 @@ add_action('template_redirect', function() {
     }
 });
 
-// Removed download-related includes and functions as they have been moved to the player plugin
+/*--------------------------------------------------------------
+# Registration Email Blocking
+--------------------------------------------------------------*/
 
+/**
+ * Get blocked emails from file
+ *
+ * @return array
+ */
 function get_blocked_emails() {
     static $blocked_emails = null;
     if ($blocked_emails === null) {
         $file = get_stylesheet_directory() . '/blocked-emails.txt';
-        $blocked_emails = file_exists($file) ?
-            array_map('strtolower', array_map('trim', file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES))) :
-            array();
+        $blocked_emails = file_exists($file)
+            ? array_map('strtolower', array_map('trim', file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)))
+            : array();
     }
     return $blocked_emails;
 }
 
+/**
+ * Block specific emails from registering
+ *
+ * @param WP_Error $errors
+ * @param string $sanitized_user_login
+ * @param string $user_email
+ * @return WP_Error
+ */
 function block_specific_emails($errors, $sanitized_user_login, $user_email) {
     $blocked_emails = get_blocked_emails();
     if (in_array(strtolower($user_email), $blocked_emails)) {
@@ -73,20 +97,40 @@ function block_specific_emails($errors, $sanitized_user_login, $user_email) {
 }
 add_filter('registration_errors', 'block_specific_emails', 10, 3);
 
+/*--------------------------------------------------------------
+# Page Template Styles
+--------------------------------------------------------------*/
+
+/**
+ * Enqueue CSS for page-list-cat template
+ */
 function enqueue_page_list_cat_css() {
     if (is_page_template('page-list-cat.php')) {
-        wp_enqueue_style('page-list-cat-style', get_stylesheet_directory_uri() . '/page-list-cat.css', array(), '1.0');
+        wp_enqueue_style(
+            'page-list-cat-style',
+            get_stylesheet_directory_uri() . '/page-list-cat.css',
+            array(),
+            '1.0'
+        );
     }
 }
 add_action('wp_enqueue_scripts', 'enqueue_page_list_cat_css');
 
+/*--------------------------------------------------------------
+# User Admin Columns
+--------------------------------------------------------------*/
 
-// Add registration date column to users list
+/**
+ * Add registration date column to users list
+ */
 add_filter('manage_users_columns', function($columns) {
     $columns['registered'] = 'Registered';
     return $columns;
 });
 
+/**
+ * Populate registration date column
+ */
 add_filter('manage_users_custom_column', function($value, $column_name, $user_id) {
     if ($column_name == 'registered') {
         $user = get_userdata($user_id);
@@ -95,24 +139,39 @@ add_filter('manage_users_custom_column', function($value, $column_name, $user_id
     return $value;
 }, 10, 3);
 
-// Make the column sortable
+/**
+ * Make the registration date column sortable
+ */
 add_filter('manage_users_sortable_columns', function($columns) {
     $columns['registered'] = 'registered';
     return $columns;
 });
 
+/*--------------------------------------------------------------
+# Shop Page Banner
+--------------------------------------------------------------*/
+
+/**
+ * Output shop page footer notice banner
+ */
 function add_shop_page_footer_text() {
     if (is_shop()) {
-        echo '<div class="shop-notice-banner">
-            <p><strong>therob.lol</strong> never processes, stores, or has access to your financial information, which remains completely private and protected.<br>
-            All transactions are securely handled by PayPal using industry-standard encryption.<br>
-            </p>
-        </div>';
+        get_template_part('templates/shop-notice-banner');
     }
 }
 add_action('woocommerce_after_main_content', 'add_shop_page_footer_text');
 
-// Helper functions for accessing Bandfront Members plugin settings
+/*--------------------------------------------------------------
+# Bandfront Members Plugin Helpers
+--------------------------------------------------------------*/
+
+/**
+ * Get Bandfront Members plugin setting
+ *
+ * @param string $key
+ * @param mixed $default
+ * @return mixed
+ */
 function get_bandfront_setting($key, $default = null) {
     if (isset($GLOBALS['BandfrontMembers'])) {
         return $GLOBALS['BandfrontMembers']->getConfig()->get($key, $default);
@@ -120,17 +179,32 @@ function get_bandfront_setting($key, $default = null) {
     return $default;
 }
 
+/**
+ * Get Bandfront join page URL
+ *
+ * @return string
+ */
 function get_bandfront_join_page_url() {
     $page_id = get_bandfront_setting('join_page');
     return $page_id ? get_permalink($page_id) : home_url('/become-a-backer/');
 }
 
+/**
+ * Get Bandfront posts page URL
+ *
+ * @return string
+ */
 function get_bandfront_posts_page_url() {
     $page_id = get_bandfront_setting('posts_page');
     return $page_id ? get_permalink($page_id) : '';
 }
 
-// Helper functions for accessing Bandfront Members plugin
+/**
+ * Check if user is a Bandfront backer
+ *
+ * @param int|null $user_id
+ * @return bool
+ */
 function is_bandfront_backer($user_id = null) {
     if (isset($GLOBALS['BandfrontMembers'])) {
         return $GLOBALS['BandfrontMembers']->getRoles()->userHasBackstageAccess($user_id);
@@ -138,6 +212,11 @@ function is_bandfront_backer($user_id = null) {
     return false;
 }
 
+/**
+ * Get Bandfront join URL
+ *
+ * @return string
+ */
 function get_bandfront_join_url() {
     if (isset($GLOBALS['BandfrontMembers'])) {
         $config = $GLOBALS['BandfrontMembers']->getConfig();
@@ -146,25 +225,3 @@ function get_bandfront_join_url() {
     }
     return home_url('/become-a-backer/');
 }
-
-// Add debug to check if WooCommerce product loop is running
-add_action('woocommerce_before_shop_loop', function() {
-    error_log('[BFP DEBUG] woocommerce_before_shop_loop fired');
-});
-
-add_action('woocommerce_shop_loop', function() {
-    error_log('[BFP DEBUG] woocommerce_shop_loop fired');
-});
-
-add_action('woocommerce_after_shop_loop_item', function() {
-    error_log('[BFP DEBUG] woocommerce_after_shop_loop_item fired for product: ' . get_the_ID());
-}, 5); // Before our hook
-
-add_action('init', function() {
-    if (is_front_page()) {
-        error_log('[BFP DEBUG] On front page');
-    }
-    if (is_shop()) {
-        error_log('[BFP DEBUG] On shop page');
-    }
-});
---
+++ templates/shop-notice-banner.php
@@ -0,0 +1,6 @@
+<div class="shop-notice-banner">
+    <p>
+        <strong>therob.lol</strong> never processes, stores, or has access to your financial information, which remains completely private and protected.<br>
+        All transactions are securely handled by PayPal using industry-standard encryption.<br>
+    </p>
+</div>
