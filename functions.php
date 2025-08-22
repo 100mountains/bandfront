62c0ce10c5e1b1d701cda27a6fcce77c3cbe0782
100mountains <root@100mountains.uk>
2025-08-22 21:26:50 +0000

    Remove unused Bandfront Members helper functions; no references found. Also keep CSS edits.

--- functions.php
+++ functions.php
@@ -36,13 +36,6 @@ add_action('wp_enqueue_scripts', 'storefront_child_enqueue_styles', 11);
 --------------------------------------------------------------*/
 
 /**
- * Disable WooCommerce product image zoom
- */
-add_action('wp', function() {
-    remove_theme_support('wc-product-gallery-zoom');
-});
-
-/**
  * Remove WooCommerce breadcrumbs from Storefront theme
  */
 add_action('init', 'storefront_remove_storefront_breadcrumbs');
@@ -50,15 +43,6 @@ function storefront_remove_storefront_breadcrumbs() {
     remove_action('storefront_before_content', 'woocommerce_breadcrumb', 10);
 }
 
-/**
- * Redirect only logged-out users trying to access my-account page
- */
-add_action('template_redirect', function() {
-    if (is_account_page() && !is_user_logged_in()) {
-        wp_redirect(home_url('/login/'));
-        exit;
-    }
-});
 
 /*--------------------------------------------------------------
 # Registration Email Blocking
@@ -102,37 +86,6 @@ add_filter('registration_errors', 'block_specific_emails', 10, 3);
 --------------------------------------------------------------*/
 
 /*--------------------------------------------------------------
-# User Admin Columns
---------------------------------------------------------------*/
-
-/**
- * Add registration date column to users list
- */
-add_filter('manage_users_columns', function($columns) {
-    $columns['registered'] = 'Registered';
-    return $columns;
-});
-
-/**
- * Populate registration date column
- */
-add_filter('manage_users_custom_column', function($value, $column_name, $user_id) {
-    if ($column_name == 'registered') {
-        $user = get_userdata($user_id);
-        return $user->user_registered;
-    }
-    return $value;
-}, 10, 3);
-
-/**
- * Make the registration date column sortable
- */
-add_filter('manage_users_sortable_columns', function($columns) {
-    $columns['registered'] = 'registered';
-    return $columns;
-});
-
-/*--------------------------------------------------------------
 # Shop Page Banner
 --------------------------------------------------------------*/
 
@@ -146,67 +99,4 @@ function add_shop_page_footer_text() {
 }
 add_action('woocommerce_after_main_content', 'add_shop_page_footer_text');
 
-/*--------------------------------------------------------------
-# Bandfront Members Plugin Helpers
---------------------------------------------------------------*/
-
-/**
- * Get Bandfront Members plugin setting
- *
- * @param string $key
- * @param mixed $default
- * @return mixed
- */
-function get_bandfront_setting($key, $default = null) {
-    if (isset($GLOBALS['BandfrontMembers'])) {
-        return $GLOBALS['BandfrontMembers']->getConfig()->get($key, $default);
-    }
-    return $default;
-}
-
-/**
- * Get Bandfront join page URL
- *
- * @return string
- */
-function get_bandfront_join_page_url() {
-    $page_id = get_bandfront_setting('join_page');
-    return $page_id ? get_permalink($page_id) : home_url('/become-a-backer/');
-}
-
-/**
- * Get Bandfront posts page URL
- *
- * @return string
- */
-function get_bandfront_posts_page_url() {
-    $page_id = get_bandfront_setting('posts_page');
-    return $page_id ? get_permalink($page_id) : '';
-}
-
-/**
- * Check if user is a Bandfront backer
- *
- * @param int|null $user_id
- * @return bool
- */
-function is_bandfront_backer($user_id = null) {
-    if (isset($GLOBALS['BandfrontMembers'])) {
-        return $GLOBALS['BandfrontMembers']->getRoles()->userHasBackstageAccess($user_id);
-    }
-    return false;
-}
-
-/**
- * Get Bandfront join URL
- *
- * @return string
- */
-function get_bandfront_join_url() {
-    if (isset($GLOBALS['BandfrontMembers'])) {
-        $config = $GLOBALS['BandfrontMembers']->getConfig();
-        $page_id = $config->get('join_page');
-        return $page_id ? get_permalink($page_id) : home_url('/become-a-backer/');
-    }
-    return home_url('/become-a-backer/');
-}
+ 
